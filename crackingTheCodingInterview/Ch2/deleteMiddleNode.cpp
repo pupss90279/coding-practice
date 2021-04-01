b0ed72d3a9d35fd8e7496b9c343048f0bb4a371e
pupss90279 <ycwuaa@connect.ust.hk>
2021-03-31 23:24:03 -0700

    add delete middle node

--- crackingTheCodingInterview/Ch2/deleteMiddleNode.cpp
+++ crackingTheCodingInterview/Ch2/deleteMiddleNode.cpp
@@ -10,7 +10,44 @@
 #include "node.h"
 using namespace std;
 
+void deleteMiddleNode(Node* head, Node* nodeToDelete) {
+
+	if(!head || !nodeToDelete) return;
+
+	Node* iter = head;
+
+	while(iter) {
+
+		if(iter->next == nodeToDelete) {
+
+			iter->next = iter->next->next;
+			delete nodeToDelete;
+			break;
+		}
+
+		iter = iter->next;
+	}
+}
+
 int main() {
 
-	
+	vector<int> test1{5, 4, 3, 2, 1};
+	Node* head = createLinkedList(test1);
+	printLinkedList(head);
+
+	deleteMiddleNode(head, head->next->next);
+	printLinkedList(head);
+
+	Node* newNode = new Node;
+	deleteMiddleNode(head, newNode);
+	printLinkedList(head);
+
+	deleteMiddleNode(head, head);
+	printLinkedList(head);
+
+	deleteMiddleNode(head, NULL);
+	printLinkedList(head);
+
+	deleteMiddleNode(head, head->next);
+	printLinkedList(head);
 }
